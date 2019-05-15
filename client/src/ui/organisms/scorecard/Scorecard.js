bba7cb4658e25d3f7c4270840aa44a6a251043a9
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-05-15 15:29:29 +1000

    All scorecard content

--- client/src/ui/organisms/scorecard/Scorecard.js
+++ client/src/ui/organisms/scorecard/Scorecard.js
@@ -6,21 +6,24 @@ import Cairns from './cairns';
 import Mackay from './mackay';
 import Toowoomba from './toowoomba';
 import Townsville from './townsville';
+import Redcliffe from './redcliffe';
 
 class Scorecard extends Component {
   render() {
     if (this.props.location === 'Queensland') {
       return <Default backgroundColor={this.props.backgroundColor} />;
-    } else if (this.props.location === 'Redcliffe') {
-      return <div />;
     } else if (this.props.location === 'Mackay') {
       return <Mackay backgroundColor={this.props.backgroundColor} />;
+    } else if (this.props.location === 'Redcliffe') {
+      return <Redcliffe backgroundColor={this.props.backgroundColor} />;
     } else if (this.props.location === 'Cairns') {
       return <Cairns backgroundColor={this.props.backgroundColor} />;
     } else if (this.props.location === 'Toowoomba') {
       return <Toowoomba backgroundColor={this.props.backgroundColor} />;
     } else if (this.props.location === 'Townsville') {
       return <Townsville backgroundColor={this.props.backgroundColor} />;
+    } else if (this.props.location === 'Gold Coast') {
+      return <GoldCoast backgroundColor={this.props.backgroundColor} />;
     }
   }
 }
